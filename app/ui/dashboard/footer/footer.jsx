195233fbb504942f1a6508723c63ffb9e01856d0
A-Allaoua <100159350+A-Allaoua@users.noreply.github.com>
2024-05-24 20:23:57 +0100

    edit users page

---
+++ app/ui/dashboard/footer/footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './footer.module.css';
+
+function Footer() {
+  return (
+    <div className={styles.container}>
+        <div className={styles.logo}>Hilli Tech</div>
+        <div className={styles.ext}>All right reserved.</div>
+      
+    </div>
+  )
+}
+
+export default Footer
